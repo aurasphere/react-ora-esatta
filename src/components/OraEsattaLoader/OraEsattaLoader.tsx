7ed0a347ebe1a3f2b8245f9323ce4a61ac85fb1b
Donato Rimenti <donatohan.rimenti@gmail.com>
2024-01-18 14:08:29 +0100

    First commit

---
+++ src/components/OraEsattaLoader/OraEsattaLoader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function OraEsattaLoader(): JSX.Element {
+  return (
+    <video
+      src="https://github.com/aurasphere/react-ora-esatta-loader/raw/main/assets/ora-esatta.mp4"
+      loop
+      autoPlay
+    />
+  );
+}
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as OraEsattaLoader } from "./OraEsattaLoader";
